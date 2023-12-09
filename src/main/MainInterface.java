542b068782e80da4f5cf10cfc9dd31a1eccb6760
wysha <wyshazhisishen@yeah.net>
2023-12-10 04:46:47 +0800

    1.0.0

--- src/main/MainInterface.java
+++ src/main/MainInterface.java
@@ -1,7 +1,24 @@
 package main;
 
-public class MainInterface {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
 
+public class MainInterface extends JFrame {
+    public static MainInterface mainInterface=new MainInterface();
+    private JPanel contentPane;
+
+    public MainInterface(){
+        setContentPane(contentPane);
+        mainInterface=this;
+        setSize(
+                (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth()*2/3),
+                (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight()*2/3)
+        );
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        mainInterface.setVisible(
+                true
+        );
     }
 }
